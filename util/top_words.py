7f263c0870445a0cd52e0cceebfa281ac4309729
Glenn M. Davis <gmdavis1@gmail.com>
2018-11-18 21:24:26 -0800

    working with stats SE data

--- util/top_words.py
+++ util/top_words.py
@@ -18,16 +18,22 @@ DATE_INDEX = 7
 POSTTYPEID_INDEX = 17
 TAG_INDEX = 19
 
+# number of top words (by tf-idf) to extract from each postbody
 NUM_TOP_WORDS = 5
 
+# remove html tags from postbodies
 TAG_RE = re.compile(r'<[^>]+>')
 def clean(text):
     return TAG_RE.sub('', text)
 
+# filepaths
 if len(sys.argv) > 1:
     CSV_FILE = sys.argv[1]
 else:
-    CSV_FILE = '../data/stats.stackexchange.com/Posts.csv'
+    CSV_FILE = '../data/stats.stackexchange.com/Posts_2.csv'
+
+# if using a split CSV, the index of the first data row, else 0
+START_ROW = 200000
 
 OUTPUT_FILE = Path(CSV_FILE).with_name(Path(CSV_FILE).with_suffix('').name + '-top_words_more').with_suffix('.tsv')
 
@@ -50,9 +56,9 @@ with open(CSV_FILE, "r") as f:
 
 # reference is a dict of (postbody, postid, userid) with row as key
 # corpus is a list of postbodies, index is the row the postbody came from
-assert corpus[0] == reference[0][0]
-assert corpus[100] == reference[100][0]
-assert corpus[8000] == reference[8000][0]
+assert corpus[0] == reference[START_ROW + 0][0]
+assert corpus[100] == reference[START_ROW + 100][0]
+assert corpus[8000] == reference[START_ROW + 8000][0]
 
 print('Reference dict built.')
 
@@ -82,12 +88,10 @@ for num, row in enumerate(tqdm(dense)):
     post_body = row.tolist()[0]
     phrase_scores = [pair for pair in zip(range(0, len(post_body)), post_body) if pair[1] > 0]
     sorted_phrase_scores = sorted(phrase_scores, key = lambda t: t[1] * -1)
-    # print('---ROW {}---'.format(num))
     top_phrases = []
     for phrase, score in [(feature_names[word_id], score) for (word_id, score) in sorted_phrase_scores][:NUM_TOP_WORDS]:
-        # print('{0: <20} {1}'.format(phrase, score))
         top_phrases.append(phrase)
-    output.append(reference[num] + top_phrases)
+    output.append(reference[START_ROW + num] + top_phrases)
 
 ########## output to file ##########
 
