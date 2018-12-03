0188bcaa2ec64d97748289387743dac5c0b6d26f
cjyuan8 <christinajingyuan@gmail.com>
2018-12-02 16:59:11 -0800

    Add function for computing modularity

---
+++ util/get_modularity.py
@@ -0,0 +1,21 @@
+def get_modularity(G, community_dict):
+    '''
+    This function might be useful to compute the modularity of a given cut
+    defined by two sets S and neg_S. We would normally require sets S and neg_S
+    to be disjoint and to include all nodes in Graph.
+    
+    - community_dict: maps node id to community
+    '''
+    ##########################################################################
+    A = get_adjacency_matrix(G)
+    two_M = float(np.sum(A))
+    mod_sum = 0
+    for NI in G.Nodes():
+        NI_id = NI.GetId()
+        for NJ in G.Nodes():
+            NJ_id = NJ.GetId()
+            if (community_dict[NI_id] == community_dict[NJ_id]):
+                mod_sum += A[NI_id, NJ_id] - ((NI.GetDeg() * NJ.GetDeg()) / two_M)
+    modularity = mod_sum / two_M
+    return modularity
+    ##########################################################################
